d7891f5f6f8ba73b7fb08f22fe721aec531867f9
M Blydenburgh <mike.blydenburgh@gmail.com>
2019-02-09 18:01:07 -0500

    shuffle cards on click

--- src/App.js
+++ src/App.js
@@ -2,35 +2,49 @@ import React, { Component } from 'react';
 import Styled from 'styled-components';
 import Nav from './components/Nav/Nav';
 import Hero from './components/Hero/Hero';
-import CardsWrapper from './components/CardsWrapper/CardsWrapper'
+import CardsWrapper from './components/CardsWrapper/CardsWrapper';
 
 class App extends Component {
   state = {
     score: 0,
     topScore: 0,
-    cards:[
-        {alt:"1",clicked:false},
-        {alt:"2",clicked:false},
-        {alt:"3",clicked:false},
-        {alt:"4",clicked:false},
-        {alt:"5",clicked:false},
-        {alt:"6",clicked:false},
-      ]
+    cards: [
+      { alt: '1', clicked: false },
+      { alt: '2', clicked: false },
+      { alt: '3', clicked: false },
+      { alt: '4', clicked: false },
+      { alt: '5', clicked: false },
+      { alt: '6', clicked: false },
+    ],
+  }
+
+  // Fisher-Yates sorting algorithm
+  shuffle = (a) => {
+    let j; let x; let i;
+    for (i = a.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = a[i];
+      a[i] = a[j];
+      a[j] = x;
+    }
+    return a;
   }
 
   handleClick = (id) => {
     const newCards = [...this.state.cards];
-    const {clicked} = newCards[id];
-    
+    const { clicked } = newCards[id];
+
     console.log(`clicked ${id}`);
-    if(!clicked){
+    if (!clicked) {
       newCards[id].clicked = true;
-      this.setState({
-        score: this.state.score+1,
-        cards: newCards});
-    }
-    else{
-      console.log(`already clicked`);
+      const shuffledCards = this.shuffle(newCards);
+      this.setState(prevState => ({
+        score: prevState.score + 1,
+        cards: shuffledCards,
+      }));
+    } else {
+      console.log('already clicked');
+      this.setState({ score: 0 });
     }
   }
 
